e33ea54eaa8377940789c27bb70b9df879335197
Abrorbek Ismoilov <107830282+abror2142@users.noreply.github.com>
2025-06-10 10:36:10 +0500

    Development mode configuration

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Changed NOW Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -6,4 +6,4 @@ php artisan migrate:fresh --seed --force
 chown -R www-data:www-data /api /api/vendor
 chmod -R 775 /api /api/vendor
 
-exec php-fpm
+exec "$@"
